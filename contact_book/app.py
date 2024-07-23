b28bd8f2a0b3a796a713be699dbaf320f2e28fe6
joekariuki3 <joekariuki3@gmail.com>
2024-07-23 19:00:18 +0300

    Added add_new_contact function that is called when a user chooses an option to add a contact, then prompts the user for contact information then saves the information and prints out name and phone number on success
    added options that will resemble options chosen by user and call respective function as per  the option selected
    
    Refactored the main function to include the new changes (options, functions)
    
    added doc string to functions to explain how they work

--- contact_book/app.py
+++ contact_book/app.py
@@ -4,12 +4,60 @@
 
 from contact_book_model import ContactBook
 
-def main():
-    print("Contact book app")
-    contact_book = ContactBook()
-    # test add contacts
-    new_contact_id = contact_book.add_contact('John', '555-555-5555', 'nTl6w@example.com', '123 Main St')
-    print(new_contact_id)
+def add_new_contact():
+    """
+    Adds a new contact to the contact book.
 
+    This function prompts the user to enter the contact's name, phone number, email, and address.
+    It then creates a new `ContactBook` instance and adds the contact to it.
+    Finally, it prints a message indicating that the contact has been added, along with the contact's name and phone number.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    name = input('Enter the contact name: ')
+    phone = input('Enter the contact phone number: ')
+    email = input('Enter the contact email: ')
+    address = input('Enter the contact address: ')
+    contact = ContactBook()
+    new_contact = contact.add_contact(name=name, phone=phone, email=email, address=address)
+    print(f'New contact added: ({new_contact.name}: {new_contact.phone})')
+
+def terminate() -> None:
+    """
+    Terminate message for the program.
+    """
+    print('Goodbye!')
+
+options = {
+    1: ('New Contact', lambda: add_new_contact()),
+    2: ('Edit Contact', lambda: print('Editing a contact is not supported yet. coming soon')),
+    3: ('View Contact', lambda: print('Viewing a contact is not supported yet. coming soon')),
+    4: ('Delete Contact', lambda: print('Deleting a contact is not supported yet. coming soon')),
+    5: ('Exit', lambda: terminate()),
+}
+def main():
+    """
+    A function that runs the Contact book app, displaying options, taking user input,
+    and executing the selected option until the user decides to exit.
+    It handles user input validation and option execution, providing feedback on invalid inputs.
+    """
+    print('Contact book app')
+    running = True
+    while running:
+        for option in options:
+            print(f'[{option}] {options[option][0]}', end=' | ')
+        try:
+            option = int(input('\nEnter an option: '))
+            if option < 1 or option > len(options):
+                raise ValueError
+            options[option][1]()
+            if option == 5:
+                running = False
+        except:
+            print('Invalid option. Exiting.')
 if __name__ == '__main__':
     main()
