d2c924ed8aa7d3b35ecbee8c21db249aaae787b0
joekariuki3 <joekariuki3@gmail.com>
2024-07-24 00:01:06 +0300

    implemented the remaining crud  functions in the main function. now users are able to choose all the options and perform an operation

--- contact_book/app.py
+++ contact_book/app.py
@@ -26,6 +26,86 @@ def add_new_contact():
     new_contact = contact.add_contact(name=name, phone=phone, email=email, address=address)
     print(f'New contact added: ({new_contact.name}: {new_contact.phone})')
 
+def view_contact():
+    """
+    Prints a contact in the contact book.
+
+    This function creates a new `ContactBook` instance and prints a contact in the contact book.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    contact_id = int(input('Enter contact id: '))
+    contact = ContactBook()
+    current_contact = contact.get_contact(contact_id)
+    if current_contact:
+        print(f'Contact: {current_contact.to_dict()}')
+    else:
+        print('Contact not found')
+
+def view_all_contacts():
+    """
+    Prints all contacts in the contact book.
+
+    This function creates a new `ContactBook` instance and prints all the contacts in the contact book.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    contact = ContactBook()
+    contacts = contact.get_all_contacts()
+    for index, contact in enumerate(contacts):
+        print(contact)
+
+def edit_contact():
+    # enter contact id pass it to update_contact
+    contact_id = int(input('Enter contact id: '))
+    contact = ContactBook()
+    current_contact = contact.get_contact(contact_id)
+    if current_contact:
+        print(f'Current contact: {current_contact.to_dict()}')
+        name = input(f'Enter new name ({current_contact.name}): ')
+        phone = input(f'Enter new phone ({current_contact.phone}): ')
+        email = input(f'Enter new email ({current_contact.email}): ')
+        address = input(f'Enter new address ({current_contact.address}): ')
+        if not name:
+            name = current_contact.name
+        if not phone:
+            phone = current_contact.phone
+        if not email:
+            email = current_contact.email
+        if not address:
+            address = current_contact.address
+        contact.update_contact(contact_id, name, email, phone, address)
+        print('Contact updated successfully')
+        print(f'Updated contact: {current_contact.to_dict()}')
+    else:
+        print('Contact not found')
+
+def delete_contact():
+    """
+    Deletes a contact from the contact book.
+
+    This function prompts the user to enter the contact ID of the contact to be deleted. It then creates a new `ContactBook` instance and calls the `delete_contact` method on it, passing in the contact ID. Finally, it prints a message indicating that the contact has been deleted successfully.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    contact_id = int(input('Enter contact id: '))
+    contact = ContactBook()
+    contact.delete_contact(contact_id)
+    print('Contact deleted successfully')
+
+
 def terminate() -> None:
     """
     Terminate message for the program.
@@ -34,10 +114,11 @@ def terminate() -> None:
 
 options = {
     1: ('New Contact', lambda: add_new_contact()),
-    2: ('Edit Contact', lambda: print('Editing a contact is not supported yet. coming soon')),
-    3: ('View Contact', lambda: print('Viewing a contact is not supported yet. coming soon')),
-    4: ('Delete Contact', lambda: print('Deleting a contact is not supported yet. coming soon')),
-    5: ('Exit', lambda: terminate()),
+    2: ('Edit Contact', lambda: edit_contact()),
+    3: ('View Contact', lambda: view_contact()),
+    4: ('View Contacts', lambda: view_all_contacts()),
+    5: ('Delete Contact', lambda: delete_contact()),
+    6: ('Exit', lambda: terminate()),
 }
 def main():
     """
@@ -47,6 +128,7 @@ def main():
     """
     print('Contact book app')
     running = True
+    exit_option = len(options)
     while running:
         for option in options:
             print(f'[{option}] {options[option][0]}', end=' | ')
@@ -55,9 +137,9 @@ def main():
             if option < 1 or option > len(options):
                 raise ValueError
             options[option][1]()
-            if option == 5:
+            if option == exit_option:
                 running = False
-        except:
-            print('Invalid option. Exiting.')
+        except Exception as error:
+            print(f'Invalid option. Exiting.{error}')
 if __name__ == '__main__':
     main()
